cf2d2fcbb95c946b8f425394b304aab11cb0e3fd
Manikanta K <52909729+manikanta-0403@users.noreply.github.com>
2023-06-21 13:27:44 +0530

    updated the code for select chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartAComponent } from './chart-a/chart-a.component';
+import { ChartBComponent } from './chart-b/chart-b.component';
+import { ChartCComponent } from './chart-c/chart-c.component';
+import { ChartDComponent } from './chart-d/chart-d.component';
+import { MapboxComponent } from './mapbox/mapbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chart1', component: ChartAComponent
+  },
+  {
+    path: 'chart2', component: ChartBComponent
+  },
+  {
+    path: 'chart3', component: ChartCComponent
+  },
+  {
+    path: 'chart4', component: ChartDComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { ChartBComponent } from './chart-b/chart-b.component';
 import { FormsModule } from '@angular/forms';
 import { ChartCComponent } from './chart-c/chart-c.component';
 import { ChartDComponent } from './chart-d/chart-d.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { NgFor } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -17,14 +22,19 @@ import { ChartDComponent } from './chart-d/chart-d.component';
     ChartAComponent,
     ChartBComponent,
     ChartCComponent,
-    ChartDComponent
+    ChartDComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    NgFor,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
